4fecca0dad863d99b950bc9e449a5f470b046536
Vignesh Jaganathan <50860739+514839@users.noreply.github.com>
2024-07-18 19:21:42 +0530

    Create FrontendTest.java

---
+++ src/test/java/org/tektutor/FrontendTest.java
@@ -0,0 +1,31 @@
+package org.tektutor;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.After;
+import static org.junit.Assert.*;
+
+public class FrontendTest {
+	String actualResponse;
+	String expectedResponse;
+	Frontend fe;
+
+	@Before
+	public void init() {
+		fe = new Frontend();	
+	}
+
+  @Test
+	public void testGetModuleName() throws Exception{
+		actualResponse   = fe.getModuleName();
+		expectedResponse = "Frontend";
+
+		assertEquals ( expectedResponse, actualResponse );
+		Thread.sleep(30 * 1000);
+	}
+
+	@After
+	public void cleanup() {
+		fe = null;	
+	}
+}
